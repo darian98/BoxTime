efab0d246cd22ad173650bf78bb04ad4d955b01b
Darian Hanci <darian@fddb.de>
2025-11-05 16:54:54 +0100

    First commit

---
+++ BoxTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxTimeApp.swift
+//  BoxTime
+//
+//  Created by Darian Hanci on 05.11.25.
+//
+
+import SwiftUI
+
+@main
+struct BoxTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
