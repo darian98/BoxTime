45256400d4a5127d966585a3fadc7166586d4888
Darian Hanci <darian@fddb.de>
2025-11-06 07:05:28 +0100

    added trainingsession and exercise models, added rounds

---
+++ UI/Home/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  BoxTime
+//
+//  Created by Darian Hanci on 05.11.25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @StateObject var viewModel = HomeViewModel()
+    @StateObject var timerViewModel = TimerViewModel()
+    
+    var body: some View {
+        VStack {
+            
+            TabView(selection: $viewModel.activeTab) {
+                TimerView(viewModel: timerViewModel)
+                    .tabItem { Label("Timer", systemImage: "clock") }
+                    .tag(AppTab.tab1)
+                
+                SettingsView(timerViewModel: timerViewModel)
+                    .tabItem { Label("Settings", systemImage: "gearshape") }
+                    .tag(AppTab.tab2)
+                
+            }
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
